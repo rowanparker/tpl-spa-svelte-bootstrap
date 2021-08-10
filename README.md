60c915b3676f8f8bf707d8878a8092269ca3524c
Rowan Parker <rowanparker@gmail.com>
2021-08-10 14:01:17 +1000

    Initial commit without Bootstrap.

---
+++ README.md
@@ -0,0 +1,5 @@
+Default [Svelte template](https://github.com/sveltejs/template) with:
+
+- Docker container
+- Already converted to TypeScript
+- [Bootstrap](https://getbootstrap.com/) already integrated
---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Svelte SPA Template</h1>
+	<p>
+		Docker + Svelte
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+</style>
